f4d77aaea289ddc60f091a797f7098ffd676158e
vida <m.vidamohammadi@gmail.com>
2025-08-12 18:26:50 -0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Import Vuetify و استایل‌ها
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify({})
+
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
