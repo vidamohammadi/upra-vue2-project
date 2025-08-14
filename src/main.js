5d4d77115f20d490fa25128e2a4d85984d1d84c0
vida <m.vidamohammadi@gmail.com>
2025-08-13 18:45:09 -0800

    add: use dana font

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-// Import Vuetify و استایل‌ها
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@/assets/fonts.css'
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false
 
