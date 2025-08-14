3bf3d0cf2e6093e7638c8e2f974f8dc241f623e3
vida <m.vidamohammadi@gmail.com>
2025-08-14 01:35:06 -0800

    fix: fix header for mobile display

--- src/components/header/MainNavbar.vue
+++ src/components/header/MainNavbar.vue
@@ -1,25 +1,38 @@
 <template>
   <v-row class="pa-5 d-flex justify-space-between">
-    <v-col>
+    <v-col lg="6" cols="9">
       <v-btn
         class="white--text rounded-lg px-2 mx-3"
-        style="background-color: #ff6633">
+        style="background-color: #ff6633"
+        v-if="isDesktop">
         <v-icon size="20" color="white">mdi-chevron-down</v-icon>
         پروفایل
+
         <v-icon size="20" color="white" class="ml-1">
           mdi-account-circle-outline
         </v-icon>
       </v-btn>
       <v-btn
+        v-else
+        class="white--text rounded-lg mx-1"
+        style="background-color: #ff6633">
+        ورود
+      </v-btn>
+      <v-btn
         style="background-color: #1b6be5"
-        class="white--text rounded px-15">
+        class="white--text rounded-lg px-lg-15 px-1">
         پرداخت خودکار
         <v-icon size="20" color="white" class="ml-1">mdi-credit-card</v-icon>
       </v-btn>
     </v-col>
-    <v-col dir="rtl" class="d-flex align-top">
-      <v-img :src="logoSrc" max-width="140px" max-height="65px"  class="mt-n3" ></v-img>
+    <v-col dir="rtl" class="d-flex align-top" lg="6" cols="3">
+      <v-img
+        :src="logoSrc"
+        :max-width="isDesktop ? '140px' : '80px'"
+        max-height="65px"
+        class="mt-n3"></v-img>
       <v-text-field
+        v-if="isDesktop"
         class="search-field mx-5"
         dense
         placeholder="جستجوی فیلم، سریال، بازیگر و کارگردان"
@@ -28,7 +41,7 @@
         hide-details
         rounded-xl
         prepend-inner-icon="mdi-magnify"></v-text-field>
-      <div class="white--text mt-2">
+      <div class="white--text mt-2" v-if="isDesktop">
         <v-icon size="20" color="white">mdi-penguin</v-icon>
         کودک
       </div>
@@ -43,6 +56,11 @@
     name: 'MainNavbar',
     data() {
       return {logoSrc};
+    },
+    computed: {
+      isDesktop() {
+        return this.$vuetify.breakpoint.mdAndUp;
+      }
     }
   };
 </script>
--- src/components/header/MainView.vue
+++ src/components/header/MainView.vue
@@ -3,12 +3,16 @@
     :src="imageSrc"
     width="100%"
     height="550px"
-    position="center top">
-<!--    <div class="gradient-overlay"></div>-->
+    position="center top"
+    :contain="isMobile">
+    <!--    <div class="gradient-overlay"></div>-->
     <MainNavbar />
-    <div
-      class="overlay-text"
-      style="width: 100%; text-align: center; margin-top: 150px">
+    <div class="overlay-text-mobile" v-if="isMobile">
+      <div>"تو تصمیم می‌گیری چقدر پرداخت کنی!"</div>
+      <div>دنیایی از فیلم‌های متنوع ایرانی و خارجی</div>
+      <div>تماشا هستید؟</div>
+    </div>
+    <div class="overlay-text" v-else>
       <div>"تو تصمیم می‌گیری چقدر پرداخت کنی!"</div>
       <div>دنیایی از فیلم‌های متنوع ایرانی و خارجی</div>
       <div>تماشا هستید؟</div>
@@ -25,6 +29,11 @@
     components: {MainNavbar},
     data() {
       return {imageSrc};
+    },
+    computed: {
+      isMobile() {
+        return this.$vuetify.breakpoint.smAndDown;
+      }
     }
   };
 </script>
@@ -44,17 +53,35 @@
     );
     z-index: 1;
   }
+  .overlay-text-mobile{
+    box-shadow: 1px 2px 4px 0px rgba(0, 0, 0, 0.5);
+    width: 100%;
+    text-align: center;
+    margin-top: 50px;
+    height: 100px;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 25px;
+    letter-spacing: 0;
+    vertical-align: middle;
+    color: white;
+    direction: rtl;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
   .overlay-text {
     box-shadow: 1px 2px 4px 0px rgba(0, 0, 0, 0.5);
-
-    width: 868px;
+    width: 100%;
+    text-align: center;
+    margin-top: 150px;
     height: 300px;
     font-weight: 700;
     font-style: normal;
     font-size: 48px;
     line-height: 100px;
     letter-spacing: 0;
-    text-align: center;
     vertical-align: middle;
     color: white;
     direction: rtl;
