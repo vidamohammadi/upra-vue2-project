b7b7e5390af84ad4e64e5b11962fd56c35884cc4
vida <m.vidamohammadi@gmail.com>
2025-08-13 18:46:20 -0800

    add: add navbar for desktop

---
+++ src/components/header/MainNavbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-row class="pa-5 d-flex justify-space-between">
+    <v-col>
+      <v-btn
+        class="white--text rounded-lg px-2 mx-3"
+        style="background-color: #ff6633">
+        <v-icon size="20" color="white">mdi-chevron-down</v-icon>
+        پروفایل
+        <v-icon size="20" color="white" class="ml-1">
+          mdi-account-circle-outline
+        </v-icon>
+      </v-btn>
+      <v-btn
+        style="background-color: #1b6be5"
+        class="white--text rounded px-15">
+        پرداخت خودکار
+        <v-icon size="20" color="white" class="ml-1">mdi-credit-card</v-icon>
+      </v-btn>
+    </v-col>
+    <v-col dir="rtl" class="d-flex align-top">
+      <v-img :src="logoSrc" max-width="140px" max-height="65px"  class="mt-n3" ></v-img>
+      <v-text-field
+        class="search-field mx-5"
+        dense
+        placeholder="جستجوی فیلم، سریال، بازیگر و کارگردان"
+        clearable
+        outlined
+        hide-details
+        rounded-xl
+        prepend-inner-icon="mdi-magnify"></v-text-field>
+      <div class="white--text mt-2">
+        <v-icon size="20" color="white">mdi-penguin</v-icon>
+        کودک
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import logoSrc from '@/assets/logo.804f1f4.svg';
+
+  export default {
+    name: 'MainNavbar',
+    data() {
+      return {logoSrc};
+    }
+  };
+</script>
+
+<style>
+  .search-field .v-icon {
+    color: white !important;
+  }
+  .search-field input::placeholder {
+    color: white !important;
+    opacity: 1 !important;
+  }
+  .v-text-field--outlined fieldset {
+    color: white !important;
+  }
+</style>
--- src/components/header/MainView.vue
+++ src/components/header/MainView.vue
@@ -3,18 +3,63 @@
     :src="imageSrc"
     width="100%"
     height="550px"
-    position="center top"></v-img>
+    position="center top">
+<!--    <div class="gradient-overlay"></div>-->
+    <MainNavbar />
+    <div
+      class="overlay-text"
+      style="width: 100%; text-align: center; margin-top: 150px">
+      <div>"تو تصمیم می‌گیری چقدر پرداخت کنی!"</div>
+      <div>دنیایی از فیلم‌های متنوع ایرانی و خارجی</div>
+      <div>تماشا هستید؟</div>
+    </div>
+  </v-img>
 </template>
 
 <script>
   import imageSrc from '@/assets/Group.jpg';
+  import MainNavbar from '@/components/header/MainNavbar.vue';
 
   export default {
     name: 'MainView',
+    components: {MainNavbar},
     data() {
       return {imageSrc};
     }
   };
 </script>
 
-<style></style>
+<style>
+  .gradient-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(
+      179.71deg,
+      rgba(0, 0, 0, 0.855) 1.85%,
+      rgba(0, 0, 0, 0) 39.23%,
+      rgba(0, 0, 0, 0.855) 102.5%
+    );
+    z-index: 1;
+  }
+  .overlay-text {
+    box-shadow: 1px 2px 4px 0px rgba(0, 0, 0, 0.5);
+
+    width: 868px;
+    height: 300px;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 48px;
+    line-height: 100px;
+    letter-spacing: 0;
+    text-align: center;
+    vertical-align: middle;
+    color: white;
+    direction: rtl;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
