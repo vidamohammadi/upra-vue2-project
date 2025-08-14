43ce1e5bba88e4a6a2ec789d9220a15f7c1e75d3
vida <m.vidamohammadi@gmail.com>
2025-08-14 02:32:54 -0800

    fix: fix favorite section for mobile display

--- src/components/favorite-section/FavoriteCard.vue
+++ src/components/favorite-section/FavoriteCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div dir="rtl">
 
-    <v-img :src="item.image" width="195px" height="290px" class="rounded-lg"></v-img>
+    <v-img :src="item.image" :width="isMobile ? '130px' : '195px'" :height="isMobile ? '194px' : '290px'" class="rounded-lg"></v-img>
     <p class="my-3">{{item.name}}</p>
   </div>
 </template>
@@ -15,6 +15,11 @@
         required: true
       }
     },
+    computed: {
+      isMobile() {
+        return this.$vuetify.breakpoint.smAndDown;
+      }
+    }
 
   };
 </script>
--- src/components/favorite-section/FavoriteSection.vue
+++ src/components/favorite-section/FavoriteSection.vue
@@ -1,18 +1,25 @@
 <template>
-  <v-container fluid class="px-10">
+  <v-container fluid class="px-lg-10 px-5">
     <v-row class="d-flex justify-space-between">
-      <v-col style="font-size: 12px">مشاهده همه</v-col>
-      <v-col dir="rtl" style="font-size: 24px">
-        <p>فیلم های برگذیده</p>
+      <v-col
+        :style="{
+          fontSize: isMobile ? '9px' : '24px'
+        }">
+        مشاهده همه
+      </v-col>
+      <v-col
+        dir="rtl"
+        :style="{
+          fontSize: isMobile ? '12px' : '24px'
+        }">
+        <p>
+          {{ isMobile ? 'عناوین پیشنهادی' : 'فیلم های برگذیده' }}
+        </p>
       </v-col>
     </v-row>
     <v-row dir="rtl">
       <div class="movies-row">
-        <div
-            v-for="(movie, index) in movies"
-            :key="index"
-            class="movie-item"
-        >
+        <div v-for="(movie, index) in movies" :key="index" :class="isMobile? 'item-mobile' : 'movie-item'">
           <FavoriteCard :item="movie" />
         </div>
       </div>
@@ -40,30 +47,38 @@
           {name: 'انگاشته', image: favoriteImage},
           {name: 'انگاشته', image: favoriteImage},
           {name: 'انگاشته', image: favoriteImage},
-          {name: 'انگاشته', image: favoriteImage},
-
+          {name: 'انگاشته', image: favoriteImage}
         ]
       };
+    },
+    computed: {
+      isMobile() {
+        return this.$vuetify.breakpoint.smAndDown;
+      }
     }
   };
 </script>
 
 <style scoped>
-.movies-row {
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: auto;
-  gap: 16px;
-  padding-bottom: 8px;
-  scroll-behavior: smooth;
-}
+  .movies-row {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    gap: 16px;
+    padding-bottom: 8px;
+    scroll-behavior: smooth;
+  }
 
-.movie-item {
-  flex: 0 0 auto;
-  width: 200px;
-}
+  .movie-item {
+    flex: 0 0 auto;
+    width: 200px;
+  }
+  .item-mobile {
+    flex: 0 0 auto;
+    width: 130px;
+  }
 
-.movies-row::-webkit-scrollbar {
-  display: none;
-}
+  .movies-row::-webkit-scrollbar {
+    display: none;
+  }
 </style>
